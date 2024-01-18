a605a431ee9755bea90b394a57137043ea062ee1
mosyocoder <128539219+mosyocoder@users.noreply.github.com>
2024-01-18 23:38:37 +0300

    change other notes edit

--- src/components/NoteCard/NoteCard.js
+++ src/components/NoteCard/NoteCard.js
@@ -1,8 +1,42 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext } from "react";
 import Markdown from "react-markdown";
 
 import "./notecard.css";
 import { NoteContext } from "../../context/NoteContext";
+
+// function NoteCard({ data }) {
+// 	const date = new Date(data.createdAt);
+
+// 	const { notes, setNotes } = useContext(NoteContext);
+
+// 	const changeEdit = () => {
+
+// 		const noteIndex = notes.findIndex((note) => note.id === data.id);
+
+// 		const updatedNote = { ...notes[noteIndex] };
+
+// 		updatedNote.edit = !updatedNote.edit;
+
+// 		const updatedNotes = [...notes];
+// 		updatedNotes[noteIndex] = updatedNote;
+
+// 		setNotes(updatedNotes);
+// 		localStorage.setItem("notes", JSON.stringify(updatedNotes));
+// 	};
+
+// 	return (
+// 		<div className="note-card">
+// 			<div className="note-card-title">
+// 				{date.toDateString() + "  " + date.toLocaleTimeString()}
+// 				<div className="note-card-buttons">
+// 					<i onClick={() => changeEdit()} className={!data.edit ? "fa-regular fa-floppy-disk" : "fa-regular fa-pen-to-square"}></i>
+// 					<i className="fa-regular fa-trash-can"></i>
+// 				</div>
+// 			</div>
+// 			{data.edit ? <Markdown>{data.text}</Markdown> : <textarea defaultValue={data.text} autoFocus className="text" />}
+// 		</div>
+// 	);
+// }
 
 function NoteCard({ data }) {
 	const date = new Date(data.createdAt);
@@ -10,14 +44,12 @@ function NoteCard({ data }) {
 	const { notes, setNotes } = useContext(NoteContext);
 
 	const changeEdit = () => {
-		const noteIndex = notes.findIndex((note) => note.id === data.id);
+		const updatedNotes = notes.map((note) => (note.id === data.id ? { ...note, edit: !note.edit } : note));
 
-		const updatedNote = { ...notes[noteIndex] };
-
-		updatedNote.edit = !updatedNote.edit;
-
-		const updatedNotes = [...notes];
-		updatedNotes[noteIndex] = updatedNote;
+		if (!data.edit) {
+			const otherNotes = updatedNotes.filter((note) => note.id !== data.id);
+			otherNotes.forEach((note) => (note.edit = false));
+		} else console.log("save");
 
 		setNotes(updatedNotes);
 		localStorage.setItem("notes", JSON.stringify(updatedNotes));
@@ -28,11 +60,11 @@ function NoteCard({ data }) {
 			<div className="note-card-title">
 				{date.toDateString() + "  " + date.toLocaleTimeString()}
 				<div className="note-card-buttons">
-					<i onClick={() => changeEdit()} className={!data.edit ? "fa-regular fa-floppy-disk" : "fa-regular fa-pen-to-square"}></i>
+					<i onClick={() => changeEdit()} className={data.edit ? "fa-regular fa-floppy-disk" : "fa-regular fa-pen-to-square"}></i>
 					<i className="fa-regular fa-trash-can"></i>
 				</div>
 			</div>
-			{data.edit ? <Markdown>{data.text}</Markdown> : <textarea defaultValue={data.text} autoFocus className="text" />}
+			{data.edit ? <textarea defaultValue={data.text} autoFocus className="text" /> : <Markdown>{data.text}</Markdown>}
 		</div>
 	);
 }
